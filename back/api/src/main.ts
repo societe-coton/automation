8067367025d27190c33e4d768b29217df9f41b6e
Pierre <gordepierre@gmail.com>
2022-09-01 17:30:14 +0200

    INIT

---
+++ back/api/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+const SWAGGER_PATH = 'swagger';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      credentials: true,
+    },
+  });
+
+  // #region Swagger
+  const config = new DocumentBuilder()
+    .setTitle('COTON AUTOMATION - API')
+    .setDescription('')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {});
+
+  SwaggerModule.setup(SWAGGER_PATH, app, document);
+  // #endregion Swagger
+
+  console.info('Listen on ' + process.env.PORT);
+  await app.listen(process.env.PORT);
+}
+bootstrap();
