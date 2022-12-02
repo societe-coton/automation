32c2b03a2c2910f69c238d5fa0f47dca1b10628a
Benjamin 1er <bgeron@coton.dev>
2022-12-02 15:04:50 +0100

    UPDATE > Send message on Slack test channel

--- back/api/src/core/core.module.ts
+++ back/api/src/core/core.module.ts
@@ -1,16 +1,18 @@
+import { HttpModule } from "@nestjs/axios";
 import { Module } from "@nestjs/common";
 
 import { GitlabService } from "./git/git.service";
 import { MailService } from "./mail/mail.service";
 import { NotionService } from "./notion/notion.service";
 import { ReviewService } from "./review/review.service";
+import { SlackService } from "./slack/slack.service";
 
 import { GitController } from "./git/git.controller";
 import { ReviewController } from "./review/review.controller";
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [ReviewController, GitController],
-  providers: [ReviewService, GitlabService, NotionService, MailService],
+  providers: [ReviewService, GitlabService, NotionService, MailService, SlackService],
 })
 export class CoreModule {}
--- back/api/src/core/review/review.controller.ts
+++ back/api/src/core/review/review.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get } from "@nestjs/common";
 
 import { MailService } from "../mail/mail.service";
+import { SlackService } from "../slack/slack.service";
 import { ReviewService } from "./review.service";
 
 @Controller("reviews")
 export class ReviewController {
   constructor(
     private readonly reviewService: ReviewService,
-    private readonly mailService: MailService
+    private readonly mailService: MailService,
+    private readonly slackService: SlackService
   ) {}
   @Get()
   async sendReviewToClient() {
-    const unsentWorkingDays = await this.reviewService.getNotSentWorkingDays();
-    return unsentWorkingDays;
+    // const unsentWorkingDays = await this.reviewService.getNotSentWorkingDays();
+    // return unsentWorkingDays;
+    const res = await this.slackService.sendSlackMessage();
+    return res;
   }
 }
